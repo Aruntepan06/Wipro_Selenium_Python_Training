4392e659f8d5d2460752d060a42391e1455e51a1
Arun Kumar Tepan <121683838+Aruntepan06@users.noreply.github.com>
2026-02-23 09:55:46 +0530

    Add files via upload

---
+++ Feb_2026_Selenium_Pytest_POM/Pages/Products_page.py
@@ -0,0 +1,17 @@
+'''
+loctors  = Page actions
+
+reusbality ,
+easy maintainnece,
+readability
+
+logon page
+1. verify ;login with valid credentails
+2. verify login with invalid credemtials
+3.Verify password masking
+4.Verify with blank crdentails
+5.Verify input text field validation:
+6 Verify password  field allowance
+7. Verify logo of the login page
+
+'''
